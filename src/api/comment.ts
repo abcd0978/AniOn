2c1baec32994b3766e3465acaa6d4147984dfaa2
ddooo00 <1995thdud@naver.com>
2023-08-21 16:11:11 +0900

    feat-api 만든거 중간저장

---
+++ src/api/comment.ts
@@ -0,0 +1,49 @@
+import supabase from "../supabaseClient";
+import { Database } from "../types/supabase";
+type PostComment = Database["public"]["Tables"]["post_comments"]["Row"];
+
+const fetchComments = async (id: string, page: number): Promise<any> => {
+  const itemsPerPage = 4; // 한 페이지당 보여줄 댓글 개수
+
+  // 해당 페이지의 시작 인덱스 계산
+  const startIndex = (page - 1) * itemsPerPage;
+
+  // 댓글 데이터 가져오기
+  const { data } = await supabase
+    .from("post_comments")
+    .select()
+    .eq("postid", id)
+    .range(startIndex, startIndex + itemsPerPage - 1);
+
+  // 총 댓글 개수 가져오기
+  const { count } = await supabase
+    .from("post_comments")
+    .select("count", { count: "exact" })
+    .eq("postid", id);
+
+  // 총 페이지 개수 계산
+  const totalPages = Math.ceil(count! / itemsPerPage);
+
+  // 데이터와 총 페이지 개수를 객체로 묶어서 반환
+  return { data, totalPages };
+};
+
+// 작성
+const addComment = async (createComment: PostComment) => {
+  await supabase.from("post_comments").insert(createComment);
+};
+
+// 삭제
+const deleteComment = async (commentId: string) => {
+  await supabase.from("post_comments").delete().eq("commentid", commentId);
+};
+
+// 수정
+const updateComment = async (editComment: PostComment) => {
+  await supabase
+    .from("post_comments")
+    .update(editComment)
+    .eq("commentid", editComment.id);
+};
+
+export { fetchComments, addComment, deleteComment, updateComment };
--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Database } from "../types/supabase";
+type PostComment = Database["public"]["Tables"]["post_comments"]["Row"];
 
 type Props = {};
 
