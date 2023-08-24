966dad856f54ca5de302a5b00a99b712c97fcc51
eunhee <eunhee9902@gmail.com>
2023-08-24 20:47:48 +0900

    pull해온거

--- src/components/myPage/WhatIWrote.tsx
+++ src/components/myPage/WhatIWrote.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-import { atom, useAtom } from "jotai"; // Import from jotai
-import { Database } from "../../types/supabase";
+import React, { useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { atom, useAtom } from 'jotai';
+import { Database } from '../../types/supabase';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("supabase의 환경변수가 없습니다.");
+  throw new Error('supabase의 환경변수가 없습니다.');
 }
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
@@ -18,31 +18,39 @@ type Post = {
   content: string | null;
 };
 
-const userPostsAtom = atom<Post[]>([]);
+const userPostsAtom = atom<Database['public']['Tables']['users']['Row'] | null>(
+  null,
+);
 
 const WhatIWrote = () => {
   const [userPosts, setUserPosts] = useAtom(userPostsAtom);
 
-  const userId = "2fb03ff7-9993-458b-8740-317a04b36c65";
+  const userId = '2fb03ff7-9993-458b-8740-317a04b36c65';
 
   useEffect(() => {
     const fetchUserPosts = async () => {
-      console.log("Fetching user posts for user ID:", userId);
+      console.log('Fetching user posts for user ID:', userId);
       const { data, error } = await supabase
-        .from("posts")
-        .select()
-        .eq("user_id", userId)
-        .order("created_at", { ascending: false });
+        .from('posts')
+        .select('*')
+        .eq('user_id', userId)
+        .order('created_at', { ascending: false });
 
       if (error) {
-        console.error("Error fetching user posts:", error);
+        console.error('fetshUserPosts에서 에러', error);
       } else {
-        console.log("User posts fetched:", data);
+        console.log('User posts fetched:', data);
         setUserPosts(data);
+        console.log(
+          'User posts fetched2(여기까지 콘솔에 잘 나온다.그런데 빈배열이 나옴):',
+          data,
+        );
       }
     };
 
-    fetchUserPosts();
+    fetchUserPosts().then(() => {
+      console.log('검사합니다.');
+    });
   }, [setUserPosts]);
 
   return (
