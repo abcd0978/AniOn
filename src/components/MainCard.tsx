dc37bb6ea88a242ef55677bf0a8de8b2b888b5bb
abcd0978 <kimmin36944@gmail.com>
2023-08-29 09:29:50 +0900

    fix: Mainpage null exception

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -15,7 +15,9 @@ const MainCard = ({ width, data, index }: Props) => {
         img_url={
           data.images.length > 1
             ? data.images[1].img_url
-            : (data.images.length > 0 ? data.images[0].img_url : data.img)
+            : data.images.length > 0
+            ? data.images[0].img_url
+            : data.img
         }
       >
         <StMainCardImgIndex>
