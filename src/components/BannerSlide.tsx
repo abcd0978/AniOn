f0ae5ea470eae740020cf737f5d769e829bb8606
Songju Jeong <js0ngwn@gmail.com>
2023-08-30 17:19:13 +0900

    pull: pull 전 중간 코드 저장 #74

--- src/components/BannerSlide.tsx
+++ src/components/BannerSlide.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React from 'react';
+import bannerWorldcup from '../assets/bannerWorldcup.svg';
+import styled from 'styled-components';
+import Banner from '../components/Banner';
 
-type Props = {};
-
-function BannerSlide({}: Props) {
-  return <div>BannerSlide</div>;
-}
-
+const BannerSlide = () => {
+  return (
+    <Banner
+      options={{ loop: true, duration: 20 }}
+      slides={[
+        <SlideContainer>
+          <img src={bannerWorldcup} alt="Slide 1" />
+        </SlideContainer>,
+      ]}
+    />
+  );
+};
 export default BannerSlide;
+
+const SlideContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
