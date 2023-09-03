4806f2d7acbe613b23c62c1787e3fae721eac8d9
eunhee <eunhee9902@gmail.com>
2023-09-04 02:33:56 +0900

    로그인 한 이메일로 재설정 링크 보내기

--- src/components/myPage/ResetPassword.tsx
+++ src/components/myPage/ResetPassword.tsx
@@ -2,38 +2,35 @@ import React, { useState } from 'react';
 import supabase from '../../supabaseClient';
 import * as userStore from '../../store/userStore';
 import { useAtom } from 'jotai';
-import { useSetAtom } from 'jotai';
+
 const PasswordReset = () => {
-  const [email, setEmail] = useState('');
   const [emailSent, setEmailSent] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
-  const [user, setUser] = useAtom(userStore.user);
-  const writeUser = useSetAtom(userStore.writeUser);
+  const [user] = useAtom(userStore.user);
 
   const handlePasswordReset = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
 
-    const { error } = await supabase.auth.resetPasswordForEmail(email);
+    if (user && user.email) {
+      const { error } = await supabase.auth.resetPasswordForEmail(user.email);
 
-    if (error) {
-      console.error('Error password reset:', error.message);
-    } else {
-      setEmailSent(true);
+      if (error) {
+        console.error('Error password reset:', error.message);
+        setErrorMessage(error.message);
+      } else {
+        setEmailSent(true);
+      }
     }
   };
 
   return (
     <div>
+      {errorMessage && <p>{errorMessage}</p>}
+
       {emailSent ? (
         <p>이메일로 전송된 링크를 확인해주세요.</p>
       ) : (
         <form onSubmit={handlePasswordReset}>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="이메일"
-          />
           <button type="submit">재설정 링크 전송</button>
         </form>
       )}
