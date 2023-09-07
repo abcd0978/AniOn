ae3a2ca33db926d23785f6acdf52915173d535ef
abcd0978 <kimmin36944@gmail.com>
2023-09-07 14:45:50 +0900

    fixed bugs

--- src/store/headerStore.ts
+++ src/store/headerStore.ts
@@ -1,2 +1,24 @@
 import { atom } from 'jotai';
-export const activeMenu = atom<string>(window.location.pathname);
+function process(path: string) {
+  switch (path) {
+    case '/': {
+      return path;
+    }
+    case '/recommend': {
+      return path;
+    }
+    case '/shop': {
+      return path;
+    }
+    case '/board': {
+      return path;
+    }
+    case '/worldcup': {
+      return path;
+    }
+    default: {
+      return '';
+    }
+  }
+}
+export const activeMenu = atom<string>(process(window.location.pathname)); // "/"
