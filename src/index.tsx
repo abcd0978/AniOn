df49681de68c94add725bd1422c376c3d3cd4ab6
mingyuKim <kimmin36944@gmail.com>
2023-09-18 16:30:22 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,8 @@ if (process.env.NODE_ENV === 'production') {
   // production에서만 사용할 수 없도록
   // console = window.console || {};
   //console.log = function no_console() {}; // console log 막기
-  console.warn = function no_console() {}; // console warning 막기
-  console.error = function () {}; // console error 막기
+  //console.warn = function no_console() {}; // console warning 막기
+  //console.error = function () {}; // console error 막기
 }
 
 const root = ReactDOM.createRoot(
