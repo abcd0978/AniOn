f71c7df0b507645aa1d6ff913420c8169389ca17
mingyuKim <kimmin36944@gmail.com>
2023-09-18 16:29:54 +0900

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 if (process.env.NODE_ENV === 'production') {
   // production에서만 사용할 수 없도록
   // console = window.console || {};
-  console.log = function no_console() {}; // console log 막기
+  //console.log = function no_console() {}; // console log 막기
   console.warn = function no_console() {}; // console warning 막기
   console.error = function () {}; // console error 막기
 }
